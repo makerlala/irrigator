15323afe7c7f41e2342485c70dc33a97490d5a20
Dumi Loghin <dumiloghin@gmail.com>
2020-02-14 23:41:00 +0800

    send ints and floats thru SPI

--- src/arduino/irrigator_board1/irrigator_board1.ino
+++ src/arduino/irrigator_board1/irrigator_board1.ino
@@ -1,11 +1,14 @@
 /**
- * Dumi Loghin - 2020
- */
+   Dumi Loghin - 2020
+   Irrigator - Arduino Board 1
+*/
 #include <SPI.h>
 
 // SPI
 volatile byte new_cmd = 0;  // command got from SPI
-volatile byte spi_ret;  // response to send to SPI
+volatile byte spi_ret;      // response to send to SPI
+
+byte cmd = 0;
 
 // Motors
 #define mrin1_pin   2
@@ -13,28 +16,35 @@ volatile byte spi_ret;  // response to send to SPI
 #define mren_pin    3 // PWM
 
 #define mlin3_pin   5
-#define mlin4_pin   7 
+#define mlin4_pin   7
 #define mlen_pin    6 // PWM
 
-// Light and Pump 
+// Light and Pump
 #define light_pin   8
 #define pump_pin    9
 
 // current sensors
-#define all_current_pin A0
-#define jetson_current_pin A1
-#define motor_current_pin A2
-#define pump_current_pin A3
+#define current_all_pin A0
+#define current_jetson_pin A1
+#define current_motors_pin A2
+#define current_pump_pin A3
 #define water_level_pin A4
 
-byte cmd = 0;
+float current_all = 0.0;
+float current_motors = 0.0;
+float current_jetson = 0.0;
+float current_pump = 0.0;
+float water_level = 0.0;
 
+byte byte_idx = 0;
+
+// timer
+#define TTH 10
 byte t = 0;
 
-#define TTH 10
 
 void setup() {
-  
+
   // motors
   pinMode(mrin1_pin, OUTPUT);
   pinMode(mrin2_pin, OUTPUT);
@@ -51,9 +61,6 @@ void setup() {
   pinMode(light_pin, OUTPUT);
   digitalWrite(light_pin, HIGH);
 
-  // current sensors
-  pinMode(motor_current_pin, INPUT);
-
   // SPI
   pinMode(MISO, OUTPUT);
   //pinMode(MOSI, INPUT);
@@ -69,17 +76,58 @@ void setup() {
   Serial.println("Setup done.");
 }
 
+byte send_value(byte idx, byte start) {
+  if (start)
+    byte_idx = 0;
+  else
+    byte_idx++;
+  if (byte_idx >= 4)
+    return 255;
+  long* ptr;
+  switch (idx) {
+    case '0': 
+      ptr = (long*)&current_all;
+      break;
+    case '1': 
+      ptr = (long*)&current_motors;
+      break;
+    case '2': 
+      ptr = (long*)&current_jetson;
+      break;
+    case '3': 
+      ptr = (long*)&current_pump;
+      break;
+    case '4': 
+      ptr = (long*)&water_level;
+      break;
+    default:
+      return 0;
+  }
+  return (byte)((*ptr) >> (8 * byte_idx));
+}
+
 // SPI interrupt routine
 ISR (SPI_STC_vect) {
-  new_cmd = SPDR;
-  SPDR = spi_ret;
-}  
+  byte c = SPDR;
+  if (c == 'r') {
+    // continue sending value
+    SPDR = send_value(new_cmd, 0);
+  }
+  else {
+    new_cmd = c;
+    if (c >= '0' && c <= '9') {
+      SPDR = send_value(new_cmd, 1);
+    }
+    else
+      SPDR = spi_ret;
+  }  
+}
 
 void stop() {
   digitalWrite(mrin1_pin, LOW);
   digitalWrite(mrin2_pin, LOW);
   digitalWrite(mlin3_pin, LOW);
-  digitalWrite(mlin4_pin, LOW); 
+  digitalWrite(mlin4_pin, LOW);
 }
 
 void go_fwd() {
@@ -87,7 +135,7 @@ void go_fwd() {
   digitalWrite(mrin1_pin, HIGH);
   digitalWrite(mrin2_pin, LOW);
   digitalWrite(mlin3_pin, HIGH);
-  digitalWrite(mlin4_pin, LOW);  
+  digitalWrite(mlin4_pin, LOW);
 }
 
 void go_back() {
@@ -95,7 +143,7 @@ void go_back() {
   digitalWrite(mrin1_pin, LOW);
   digitalWrite(mrin2_pin, HIGH);
   digitalWrite(mlin3_pin, LOW);
-  digitalWrite(mlin4_pin, HIGH);  
+  digitalWrite(mlin4_pin, HIGH);
 }
 
 void go_left() {
@@ -130,74 +178,102 @@ void off_pump() {
   digitalWrite(pump_pin, HIGH);
 }
 
+void read_sensors() {
+  word current = analogRead(current_all_pin);
+  current_all = 0.75 * current_all + 0.25 * current / 1024;
+
+  current = analogRead(current_jetson_pin);
+  current_jetson = 0.75 * current_jetson + 0.25 * current / 1024;
+
+  current = analogRead(current_motors_pin);
+  current_motors = 0.75 * current_motors + 0.25 * current / 1024;
+
+  current = analogRead(current_pump_pin);
+  current_pump = 0.75 * current_pump + 0.25 * current / 1024;
+
+  current = analogRead(water_level_pin);
+  water_level = 0.75 * water_level + 0.25 * current / 1024;
+}
+
+void print_sensors() {
+  Serial.println(current_all);
+  Serial.println(current_jetson);
+  Serial.println(current_motors);
+  Serial.println(current_pump);
+  Serial.println(water_level);
+  Serial.println("----");
+}
+
 void loop() {
 
   // if using serial to get the command -> uncomment
   /*
-  if (Serial.available() > 0) {
+    if (Serial.available() > 0) {
     new_cmd = Serial.read();
     Serial.println(cmd);
-  }
+    }
   */
 
   // SPI command
-    if (new_cmd != cmd) {
-      cmd = new_cmd;
-      spi_ret = 0;
-      switch (cmd) {
-        case 'w':
+  if (new_cmd != cmd) {
+    cmd = new_cmd;
+    spi_ret = 0;
+    switch (cmd) {
+      case 'w':
         /*
-        if (dist > 20) {
+          if (dist > 20) {
           go_fwd();
-        }
+          }
         */
-          go_fwd();
+        go_fwd();
         break;
-        case 'a':
-          go_left();
+      case 'a':
+        go_left();
         break;
-        case 'd':
-          go_right();
+      case 'd':
+        go_right();
         break;
-        case 's':
-          go_back();
+      case 's':
+        go_back();
         break;
-        case 'q':
-          stop();
+      case 'q':
+        stop();
         break;
-        case 'l':
-          on_light();
+      case 'l':
+        on_light();
         break;
-        case 'k':
-          off_light();
+      case 'k':
+        off_light();
         break;
-        case 'm':
-          on_pump();
+      case 'm':
+        on_pump();
         break;
-        case 'n':
-          off_pump();
-        break;     
-        default:
-          Serial.print("Unknown command: ");
-          Serial.println(cmd);
-          spi_ret = 1;
-      }
+      case 'n':
+        off_pump();
+        break;
+      case 'r':
+        break;
+      default:
+        Serial.print("Unknown command: ");
+        Serial.println(cmd);
+        spi_ret = 1;
     }
+  }
+
+  read_sensors();
 
   delay(100);
   t++;
   if (t == TTH) {
     t = 0;
-    int all_curr = analogRead(all_current_pin);
-    int water_level = analogRead(water_level_pin);
-    Serial.println(all_curr + " | " + water_level);    
+    print_sensors();
   }
 
-  
-  
+
+
   /*
-  if ((cmd == 'w' || cmd == 'a' || cmd == 'd') && dist <= 20) {
+    if ((cmd == 'w' || cmd == 'a' || cmd == 'd') && dist <= 20) {
     stop();
-  }
+    }
   */
 }
--- src/arduino/irrigator_board2/irrigator_board2.ino
+++ src/arduino/irrigator_board2/irrigator_board2.ino
@@ -1,38 +1,161 @@
 /**
  * Dumi Loghin - 2020
- */
+   Irrigator - Arduino Board 2
+*/
+#include <SPI.h>
 
-#define hc04_trig_pin   13
-#define hc04_echo_pin   12
-int dist = 100;
+// SPI
+volatile byte new_cmd = 0;  // command got from SPI
+volatile byte spi_ret;      // response to send to SPI
 
-#define sl_pin      A0
-#define sr_pin      A1
-int sl_val = 0;
-int sr_val = 0;
+#define hc04_1_trig_pin   4
+#define hc04_1_echo_pin   5
+#define hc04_2_trig_pin   6
+#define hc04_2_echo_pin   7
 
-int rspeed = 128;
-int lspeed = 128;
+float dist1 = 0.0;
+float dist2 = 0.0;
 
+#define enc_left_pin      2
+#define enc_right_pin     3
+
+volatile long enc_left_val = 1;
+volatile long enc_right_val = 1;
+
+// timer
+#define TTH 10
+byte t = 0;
+
+byte byte_idx = 0;
+
+void inc_left_encoder() {
+  enc_left_val++;
+}
+
+void inc_right_encoder() {
+  enc_right_val++;
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  
+  // sonars
+  pinMode(hc04_1_trig_pin, OUTPUT);
+  pinMode(hc04_1_echo_pin, INPUT);
+  pinMode(hc04_2_trig_pin, OUTPUT);
+  pinMode(hc04_2_echo_pin, INPUT);
 
-  // sonar
-  pinMode(hc04_trig_pin, OUTPUT);
-  pinMode(hc04_echo_pin, INPUT);
+  // encoders
+  attachInterrupt(digitalPinToInterrupt(enc_left_pin), inc_left_encoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(enc_right_pin), inc_right_encoder, CHANGE);
 
+  // SPI
+  pinMode(MISO, OUTPUT);
+  //pinMode(MOSI, INPUT);
+  //pinMode(SCK, INPUT);
+  //pinMode(SS, INPUT);
+  // turn on SPI in slave mode
+  SPCR |= _BV(SPE);
+  // turn on interrupts
+  SPI.attachInterrupt();
+
+  // serial - for debug
+  Serial.begin(9600);
+  Serial.println("Setup done.");
+}
+
+byte send_value(byte idx, byte start) {
+  if (start == 1)
+    byte_idx = 0;
+  else
+    byte_idx++;
+  if (byte_idx >= 4)
+    return 255;
+  long* ptr;  
+  switch (idx) {
+    case '0': // left encoder
+    ptr = (long*)&enc_left_val;
+    break;
+    case '1': // right encoder
+    ptr = (long*)&enc_right_val;
+    break;
+    case '2': // sonar 1
+    ptr = (long*)&dist1;    
+    break;
+    case '3': // sonar 2
+    ptr = (long*)&dist2;
+    break;
+    default:
+    return 0;
+  }
+  return (byte)((*ptr) >> (8 * byte_idx)); 
+}
+
+// SPI interrupt routine
+ISR (SPI_STC_vect) {
+  byte c = SPDR;
+  if (c == 'r') {
+    // continue sending float
+    SPDR = send_value(new_cmd, 0);
+  }
+  else {
+    new_cmd = c;
+    if (c >= '0' && c <= '9') {
+      SPDR = send_value(new_cmd, 1);
+    }
+    else {
+      if (c == 'z') {
+        enc_left_val = 0;
+        enc_right_val = 0;
+        SPDR = 0;
+      }
+      else
+        SPDR = spi_ret;
+    }
+  }
+}
+
+void read_sonar1() {
+  digitalWrite(hc04_1_trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(hc04_1_trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(hc04_1_trig_pin, LOW);
+  long pulseWidth = pulseIn(hc04_1_echo_pin, HIGH);
+  dist1 = 0.75 * dist1 + 0.00425 * pulseWidth;  
+}
+
+void read_sonar2() {
+  digitalWrite(hc04_2_trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(hc04_2_trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(hc04_2_trig_pin, LOW);
+  long pulseWidth = pulseIn(hc04_2_echo_pin, HIGH);
+  dist2 = 0.75 * dist2 + 0.00425 * pulseWidth;  
+}
+
+void print_sensors() {
+  Serial.println(enc_left_val);
+  Serial.println(enc_right_val);
+  Serial.println(dist1);
+  Serial.println(dist1);  
+  Serial.println("----");  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  read_sonar1();
+  read_sonar2();
+  
+  delay(100);
+  t++;
+  if (t == TTH) {
+    t = 0;
+    print_sensors();
+    Serial.println(new_cmd);
+  }
+  
 /*
-  digitalWrite(hc04_trig_pin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(hc04_trig_pin, LOW);
-  int pulseWidth = pulseIn(hc04_echo_pin, HIGH);
-  dist = (3 * dist + pulseWidth / 58.2) / 4;
+  
   sl_val = (3 * sl_val + analogRead(sl_pin)) / 4;
   sr_val = (3 * sl_val + analogRead(sr_pin)) / 4;
   */
